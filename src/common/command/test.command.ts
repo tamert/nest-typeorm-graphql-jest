5b070d838a2691f00937a1ff0262f8de0cbbb0ea
Tamer Ağaoğlu <farerock@gmail.com>
2021-09-11 13:20:52 +0300

    converted js packages to types

--- src/common/command/test.command.ts
+++ src/common/command/test.command.ts
@@ -1,9 +1,8 @@
 import { Command } from 'nestjs-command';
 import { Injectable } from '@nestjs/common';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const cliProgress = require('cli-progress');
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const _colors = require('colors');
+import { green, cyan } from 'colors/safe';
+import { SingleBar } from 'cli-progress';
+
 /**
  * note:NODE_ENV=dev npx nestjs-command sample
  */
@@ -15,11 +14,8 @@ export class TestCommand {
     })
     async create() {
         // create a new progress bar instance and use shades_classic theme
-        const bar1 = new cliProgress.SingleBar({
-            format:
-                'CLI Progress |' +
-                _colors.cyan('{bar}') +
-                '| {percentage}% || {value}/{total} Chunks || Speed: {speed}',
+        const bar1 = new SingleBar({
+            format: 'CLI Progress |' + cyan('{bar}') + '| {percentage}% || {value}/{total} Chunks || Speed: {speed}',
             barCompleteChar: '\u2588',
             barIncompleteChar: '\u2591',
             hideCursor: true,
@@ -43,6 +39,6 @@ export class TestCommand {
         await new Promise((f) => setTimeout(f, 1000));
         bar1.update(200);
         bar1.stop();
-        console.info(_colors.green('The sample command finishes with success!'));
+        console.info(green('The sample command finishes with success!'));
     }
 }
